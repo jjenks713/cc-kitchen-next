26a7fd630eb9dbaf02a47791ce37bbda1c362629
Josh Jenkin <josh.jenkin@live.com>
2023-08-04 21:31:54 -0700

    added pagination

--- components/ImageList/ImageList.js
+++ components/ImageList/ImageList.js
@@ -3,9 +3,9 @@ import Box from '@mui/material/Box';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
-
+import { Pagination, Typography } from '@mui/material';
+import usePagination from '../UsePagination';
 import { useState } from 'react';
-
 import LinearProgress from '@mui/material/LinearProgress';
 import { AllInbox } from '@mui/icons-material';
 import Image from 'next/image';
@@ -14,16 +14,32 @@ import { motion } from 'framer-motion';
 export default function GalImageList(props) {
 
     const [loading, setLoading] = useState(false)
-
     const galleryImages = props.galImages.data.galleryCollection.items;
-    const tags = props.tags;
+    /* const tags = props.tags; */
 
-    return (
-        <div className='mb-20 mt-4 mx-0 lg:mx-24'>
+    let [page, setPage] = useState(1);
+    const PER_PAGE = 10;
+  
+    const count = Math.ceil(galleryImages.length / PER_PAGE);
+    const _DATA = usePagination(galleryImages, PER_PAGE);
+  
+    const handleChange = (e, p) => {
+        setLoading(true);
+        window.scrollTo({ top: 500, left: 0, behavior: 'smooth' })
+        setPage(p);
+        _DATA.jump(p);
+        setTimeout(() => {
+            setLoading(false)
+        }, 1000)
+    };
 
+
+    console.log(galleryImages)
+    return (            
+        <div className='mb-20 mt-4 mx-0 lg:mx-24' style={{height: loading ? '1000px' : 'inherit'}}>
             {loading ?
-                <Box sx={{ width: '100%' }}>
-                    <LinearProgress color="inherit" />
+                <Box sx={{ width: '320px' }}>
+                    <LinearProgress width={'100%'} color='inherit'/>
                 </Box>
                 :
                 <>
@@ -42,46 +58,60 @@ export default function GalImageList(props) {
                             className='m-2 md:mb-10 rounded-lg md:p-10'
                         >
                             {
-                                galleryImages.map((photo) => (
+                                _DATA.currentData().map((photo) => (
                                     <>
-                                        {photo.contentfulMetadata.tags.map((tag) => (
-                                            <>
-                                                {tags === tag.name ?
-                                                    <ImageListItem className='m-5 rounded-xl relative'>
-                                                        <motion.div
-                                                            initial={{ opacity: 0 }}
-                                                            animate={{ opacity: 1 }}
-                                                        >
-                                                            <Image //eslint-disable-line
-                                                                width={100}
-                                                                height={100}
-                                                                src={photo.image.url}
-                                                                alt={photo.title}
-                                                                className='rounded-xl shadow-lg shadow-gray-700'
-                                                                layout="responsive"
-                                                                loading="lazy"
-                                                            />
-                                                        </motion.div>
-
-                                                        <ImageListItemBar
-                                                            title={photo.title}
-                                                            subtitle={photo.description}
-                                                            className="rounded-b-xl"
+                                        
+                                        <ImageListItem className='m-5 rounded-xl relative'>
+                                            
+                                                <>
+                                                    <motion.div
+                                                        initial={{ opacity: 0 }}
+                                                        animate={{ opacity: 1 }}
+                                                    >
+                                                        <Image //eslint-disable-line
+                                                            width={100}
+                                                            height={100}
+                                                            src={photo.image.url}
+                                                            alt={photo.title}
+                                                            className='rounded-xl shadow-lg shadow-gray-700'
+                                                            layout="responsive"
+                                                            loading="lazy"
                                                         />
-                                                    </ImageListItem>
-                                                    :
-                                                    null
-                                                }
-                                            </>
-                                        ))}
+                                                    </motion.div>
+
+                                                    <ImageListItemBar
+                                                        title={photo.title}
+                                                        subtitle={photo.description}
+                                                        className="rounded-b-xl"
+                                                    />
+                                                </> 
+                                            
+                                        </ImageListItem>
+    
                                     </>
                                 ))
                             }
                         </ImageList>
-                        : null
+                        : 
+                        <Box>
+                            <Typography variant='subtitle1'>
+                                Something went wrong please try and reload.
+                            </Typography>
+                        </Box>
                     }
+
+                    <Box sx={{color: 'white'}} display={'flex'} justifyContent={'center'}>
+                        <Pagination
+                            count={count}
+                            size="large"
+                            page={page}
+                            shape="rounded"
+                            onChange={handleChange}
+                            sx={{color: 'white !important', backgroundColor: 'white', borderRadius: '16px'}}
+                        />
+                    </Box>
                 </>
-            }
-        </div >
+            } 
+        </div >        
     );
 }
